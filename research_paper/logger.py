60a293ac441a36587031a1d282e7ac9685dd5a52
rrunix <ruben.rodriguez@urjc.es>
2020-05-27 16:24:50 +0200

    add logs and tmp folders; modify log level

--- research_paper/logger.py
+++ research_paper/logger.py
@@ -1,7 +1,7 @@
 import logging
 from research_paper import multiprocessing_utils
 
-logging.basicConfig(filename='research_paper/logs/exp.log', level=logging.INFO,
+logging.basicConfig(filename='research_paper/logs/exp.log', level=logging.ERROR,
                    format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
 
